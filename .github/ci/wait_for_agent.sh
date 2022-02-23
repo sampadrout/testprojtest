394c778c8fe67506b113e064fcce3f1910382aac
sampadrout <sampad.rout@gmail.com>
2022-02-23 11:46:43 +0530

    updated

--- .github/ci/wait_for_agent.sh
+++ .github/ci/wait_for_agent.sh
@@ -3,7 +3,7 @@ attempt_counter=0
 max_attempts=100
 mkdir -p build/reports/agent
 docker-compose -f MyYML.yml logs -f | tee build/reports/agent/log.txt&
-until curl -s http://localhost:8585/api/status | jq '.registered' | grep true; do
+until curl -s http://localhost:8585/api/status | jq '.fsmState' | grep Ready; do
     if [ ${attempt_counter} -eq ${max_attempts} ]; then
     echo "Agent failed to register. Terminating..."
     exit 1
